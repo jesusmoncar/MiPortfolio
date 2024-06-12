8c69a1fb846e5c40af67c1cf83dcb7d9548b34d7
jesusmoncar <jesus.montero.jmc@gmail.com>
2024-06-12 13:14:19 +0200

    todas las mejoras realizadas

--- js/verificacion.js
+++ js/verificacion.js
@@ -1,28 +1,32 @@
 'use strict';
 
-(function() {
-    window.addEventListener('load', function() {
-        var forms = document.getElementsByClassName('needs-validation');
-        var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener('submit', function(event) {
-                var alertDiv = document.getElementById('formAlert');
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                    alertDiv.classList.remove('d-none');
-                    setTimeout(function() {
-                        alertDiv.classList.add('d-none');
-                    }, 7000); // Ocultar el alert después de 7 segundos
-                } else {
-                    event.preventDefault(); // Prevent default form submission
-                    alertDiv.classList.add('d-none');
-                    var solicitudId = 'ID-' + Math.floor(Math.random() * 1000000);
-                    document.getElementById('solicitudIdDisplay').innerText = solicitudId;
-                    var successModal = new bootstrap.Modal(document.getElementById('successModal'), { backdrop: 'static' }); // Evita que se cierre el modal al hacer clic fuera de él
-                    successModal.show();
-                }
-                form.classList.add('was-validated');
-            }, false);
-        });
-    }, false);
+(function () {
+    let forms = document.querySelectorAll('.needs-validation');
+    Array.prototype.slice.call(forms).forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            } else {
+                event.preventDefault();
+                let modal = new bootstrap.Modal(document.getElementById('confirmModal'));
+                modal.show();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
 })();
+
+document.getElementById('confirmModal').addEventListener('hidden.bs.modal', function () {
+    let form = document.getElementById('contactForm');
+    if (form.checkValidity()) {
+        let alerta = document.getElementById('alerta');
+        alerta.classList.remove('d-none');
+
+        form.reset();
+        form.classList.remove('was-validated');
+        setTimeout(function () {
+            alerta.classList.add('d-none');
+        }, 5000);
+    }
+});
